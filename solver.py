20cb9acecd7c7c07ce7c7606eeaa5843e9773f9b
Mirkamol Temirkulov <mirkosht1@gmail.com>
2025-06-13 19:30:25 +0800

    Defined all different celltypes

--- solver.py
+++ solver.py
@@ -12,11 +12,35 @@ class CellType(Enum):
     REWARD1   = auto()
     REWARD2   = auto()
     ENTRY     = auto()
-    EXIT      = auto()
 
 # define the dictionary
+# x and y coordinates used (x is left-right, y is up-down)
 WORLD = {
     (0, 0): CellType.ENTRY,
-}
+    (0, 4): CellType.REWARD1,
+    (1, 1): CellType.TRAP2,
+    (1, 3): CellType.TRAP4,
+    (1, 4): CellType.TREASURE,
+    (1, 6): CellType.TRAP3,
+    (1, 8): CellType.OBSTACLE,
+    (2, 2): CellType.OBSTACLE,
+    (2, 4): CellType.OBSTACLE,
+    (2, 7): CellType.REWARD2,
+    (2, 8): CellType.TRAP1,
+    (3, 0): CellType.OBSTACLE,
+    (3, 1): CellType.REWARD1,
+    (3, 3): CellType.OBSTACLE,
+    (3, 5): CellType.TRAP3,
+    (3, 6): CellType.OBSTACLE,
+    (3, 7): CellType.TREASURE,
+    (3, 9): CellType.TREASURE,
+    (4, 2): CellType.TRAP2,
+    (4, 3): CellType.TREASURE,
+    (4, 4): CellType.OBSTACLE,
+    (4, 6): CellType.OBSTACLE,
+    (4, 7): CellType.OBSTACLE,
+    (5, 5): CellType.REWARD2
+    }
 
-
+# define the size of the world (6 rows, 10 columns)
+WORLD_SIZE = (6, 10) 
